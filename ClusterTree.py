bfb8d08f0cf61508d79fce5881a0f50252381424
Markus Neumann <markus.neumann@uzh.ch>
2016-03-08 09:45:03 +0100

    Added unit test for ClusterTree and empty BlockClusterTree.py, small changes (mostly docstrings) in ClusterTree

--- ClusterTree.py
+++ ClusterTree.py
@@ -1,18 +1,33 @@
-"""Module_Docstring"""
+#!/usr/bin/env python
+
+"""ClusterTree.py: Implementation of a binary cluster tree for 1D lists of indices.
+    Part of master thesis "Hierarchical Matrices".
+
+    Classes:
+        ClusterTree: Gives a binary cluster tree.
+"""
 
 
 class ClusterTree:
-    """Class_Docstring"""
+    """Binary cluster tree for 1D index lists.
+
+    """
     start_index = 0
     list_size = 0
+    step_size = 0
     indices = ()
     children = None
     level = 0
     child_id = 0
     leaf_size = 0
 
-    def __init__(self, indices=(), leaf_size=0, start_index=0, level=0, child_id=0, root=None):
-        """init_Docstring"""
+    def __init__(self, indices=(), leaf_size=0, step_size=0, start_index=0, level=0, child_id=0, root=None):
+        """ClusterTree(indices, leaf_size, step_size) -> ClusterTree
+
+            indices:    list of indices,                iterable;
+            leaf_size:  max size of a leaf,             int;
+            step_size:  distance between to indices,    float;
+        """
         length = len(indices)
         self.indices = indices
         self.leaf_size = leaf_size
@@ -21,6 +36,13 @@ class ClusterTree:
         self.level = level
         self.child_id = child_id
         self.root = root
+        if step_size != 0:
+            self.step_size = step_size
+        else:
+            try:
+                self.step_size = float(indices[1]-indices[0])/len(indices)
+            except IndexError:
+                self.step_size = 0
         if length > self.leaf_size:
             split = length/2
             self.children = {"first": ClusterTree(indices=indices[:split], leaf_size=self.leaf_size, start_index=0,
@@ -30,29 +52,22 @@ class ClusterTree:
                                                    child_id=2, root=self),
                              }
 
-    def step_size(self):
-        """Return the step_size of the cluster tree."""
-        if self.root:
-            return self.root.step_size()
-        else:
-            return 1./len(self.indices)
-
     def depth(self):
-        """Return the depth of the cluster."""
+        """Return the depth of the cluster.
+
+            ClusterTree.depth() -> int
+        """
         if not self.children:
             return self.level
         else:
             return max([self.children["first"].depth(), self.children["second"].depth()])
 
     def __str__(self):
-        """String representation."""
+        """Return string representation."""
         out_str = "ClusterTree with properties:\n\tIndices: " + str(self.indices) + ",\n\tDepth: " + str(self.depth()) \
             + ",\n\tLeaf size: " + str(self.leaf_size) + "."
         return out_str
 
     def __repr__(self):
+        """Representation same as __str__."""
         return str(self)
-
-if __name__ == "__main__":
-    ct = ClusterTree(indices=range(16), leaf_size=1)
-    print ct
---
+++ test_clusterTree.py
@@ -0,0 +1,8 @@
+from unittest import TestCase
+from ClusterTree import ClusterTree
+
+
+class TestClusterTree(TestCase):
+    def test_depth(self):
+        ct = ClusterTree(indices=range(16), leaf_size=1)
+        self.assertEqual(ct.depth(), 4)
