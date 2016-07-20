f3abd9e6649e367b3a135532ae10adf15b24ccba
Markus Neumann <markus.neumann@uzh.ch>
2016-07-20 12:26:30 +0200

    Docstrings fixed.

--- cluster.py
+++ cluster.py
@@ -28,26 +28,35 @@ class Cluster(object):
                 List of lists of numpy.arrays
 
         Raises:
-            TypeError:
+            TypeError: points must be list of numpy arrays and links must be list of lists of numpy arrays!
         """
         if isinstance(points[0], np.ndarray) and isinstance(links[0][0], np.ndarray):
             self.points = points
             self.links = links
-            self.diam = self.diameter()
+            self.diam = self._diameter()
         else:
             raise TypeError("points must be list of numpy arrays and links must be list of lists of numpy arrays!")
 
-    def diameter(self):
-        """
+    def _diameter(self):
+        """Compute diameter.
+
         Return the maximal Euclidean distance between two points.
-        :return: diameter (float)
+        For big lists of points this is costly. That's why the diameter is computed at creation and saved to self.diam.
+
+        Returns:
+            diameter: float.
         """
         return max([np.linalg.norm(x-y) for x in xrange(len(self.points)) for y in xrange(x+1, len(self.points))])
 
     def distance(self, other):
-        """
+        """Compute distance to other cluster.
+
         Return minimal Euclidean distance between points of self and other
-        :param other: Cluster
-        :return: distance (float)
+
+        Args:
+            other: another instance of Cluster.
+
+        Returns:
+            distance: float.
         """
         return min([np.linalg.norm(x - y) for x in self.points for y in other.points])
