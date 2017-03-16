a5cf2d8bf47fdec59965ae66d0b89c0c12a9e178
Markus Neumann <markus.neumann@uzh.ch>
2017-03-16 10:35:59 +0100

    added cross-links for numpy, scipy and matplotlib in conf.py

--- source/conf.py
+++ source/conf.py
@@ -162,7 +162,9 @@ texinfo_documents = [
 ]
 
 
-
-
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'https://docs.python.org/': None}
+intersphinx_mapping = {'https://docs.python.org/': None,
+                       'http://docs.scipy.org/doc/numpy/': None,
+                       'http://docs.scipy.org/doc/scipy/reference': None,
+                       'http://matplotlib.org': None
+                       }
