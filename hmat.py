228b4505328998a3ec82c768d6c21fe93b413d0e
Markus Neumann <markus.neumann@uzh.ch>
2017-03-20 17:37:21 +0100

    vector multiplication and matrix multiplication done needs some more tests

--- hmat.py
+++ hmat.py
@@ -9,14 +9,22 @@ class HMat(object):
     """Implement a hierarchical Matrix
     """
 
-    def __init__(self, blocks=(), content=None, shape=(), parent_index=()):
+    def __init__(self, blocks=(), content=None, shape=(), root_index=()):
         self.blocks = blocks  # This list contains the lower level HMat
         self.content = content  # If not empty, this is either a full matrix or a RMat
         self.shape = shape  # Tuple of dimensions, i.e. size of index sets
-        self.parent_index = parent_index  # Tuple of coordinates for the top-left corner in the parent matrix
+        self.root_index = root_index  # Tuple of coordinates for the top-left corner in the root matrix
 
     def __repr__(self):
-        pass
+        return '<HMat with {content}>'.format(content=self.blocks if self.blocks else self.content)
+
+    def __mul__(self, other):
+        if type(other) is numpy.ndarray:
+            return self._mul_with_vector(other)
+        elif type(other) is numpy.matrix:
+            return self._mul_with_matrix(other)
+        else:
+            raise TypeError('unsupported operand type(s) for *: {0} and {1}'.format(type(self), type(other)))
 
     def _mul_with_vector(self, other):
         """Multiply with a vector
@@ -26,32 +34,50 @@ class HMat(object):
         :return: result vector
         :rtype: numpy.array
         """
-        if self.content:
+        if self.content is not None:
             # We have content. Multiply and return
             return self.content * other
         else:
-            x_start, y_start = self.parent_index
+            if self.shape[1] != other.shape[0]:
+                raise ValueError('shapes {0.shape} and {1.shape} not aligned: '
+                                 '{0.shape[1]} (dim 1) != {1.shape[0]} (dim 0)'.format(self, other))
+            x_start, y_start = self.root_index
             res_length = self.shape[0]
             res = numpy.zeros((res_length, 1))
             for block in self.blocks:
-                x_current, y_current = block.parent_index
+                x_current, y_current = block.root_index
                 x_length, y_length = block.shape
-                in_index_start = x_start - x_current
+                in_index_start = x_current - x_start
                 in_index_end = in_index_start + x_length
-                res_index_start = y_start - y_current
+                res_index_start = y_current - y_start
                 res_index_end = res_index_start + y_length
                 res[res_index_start: res_index_end] += block * other[in_index_start: in_index_end]
             return res
 
-    def _mul_with_hmatrix(self, other):
+    def _mul_with_matrix(self, other):
         """Multiply with a matrix
 
         :param other: matrix
-        :type other: HMat
+        :type other: numpy.matrix
         :return: result matrix
         :rtype: numpy.matrix
         """
-        return self.content * other
+        if self.content is not None:
+            return self.content * other
+        else:
+            if self.shape[1] != other.shape[0]:
+                raise ValueError('shapes {0.shape} and {1.shape} not aligned: '
+                                 '{0.shape[1]} (dim 1) != {1.shape[0]} (dim 0)'.format(self, other))
+            res_shape = (self.shape[0], other.shape[1])
+            res = numpy.zeros(res_shape)
+            row_base, col_base = self.root_index
+            for block in self.blocks:
+                row_count, col_count = block.shape
+                row_current, col_current = block.root_index
+                row_start = row_current - row_base
+                row_end = row_start + row_count
+                res[row_start:row_end, :] += block * other[row_start:row_end, :]
+            return res
 
     def to_matrix(self):
         """Full matrix representation
@@ -63,9 +89,9 @@ class HMat(object):
             out_mat = numpy.empty(self.shape)
             for block in self.blocks:
                 # determine the position of the current block
-                vertical_start = block.parent_index[0]
+                vertical_start = block.root_index[0]
                 vertical_end = vertical_start + block.shape[0]
-                horizontal_start = block.parent_index[1]
+                horizontal_start = block.root_index[1]
                 horizontal_end = horizontal_start + block.shape[1]
 
                 # fill the block with recursive call
@@ -91,7 +117,7 @@ def build_hmatrix(block_cluster_tree=None, generate_rmat_function=None, generate
     """
     if block_cluster_tree.admissible:
         raise ValueError("Root of the block cluster tree is admissible, can't generate HMat from that.")
-    root = HMat(blocks=[], shape=block_cluster_tree.shape(), parent_index=(0, 0))
+    root = HMat(blocks=[], shape=block_cluster_tree.shape(), root_index=(0, 0))
     recursion_build_hmatrix(root, block_cluster_tree, generate_rmat_function, generate_full_matrix_function)
     return root
 
@@ -108,6 +134,6 @@ def recursion_build_hmatrix(current_hmat, block_cluster_tree, generate_rmat, gen
     else:
         # recursion: generate new hmatrix for every son in block cluster tree
         for son in block_cluster_tree.sons:
-            new_hmat = HMat(blocks=[], shape=son.shape(), parent_index=son.plot_info)
+            new_hmat = HMat(blocks=[], shape=son.shape(), root_index=son.plot_info)
             current_hmat.blocks.append(new_hmat)
             recursion_build_hmatrix(new_hmat, son, generate_rmat, generate_full_mat)
