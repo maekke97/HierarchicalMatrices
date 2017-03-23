1e56654296bdb2f6812ff50fa78ceee7d3235468
Markus Neumann <markus.neumann@uzh.ch>
2017-03-23 10:53:12 +0100

    try to fix rtd added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='HMatrix',
+    version='',
+    packages=['', 'tests'],
+    url='',
+    license='',
+    author='nem',
+    author_email='',
+    description=''
+)
