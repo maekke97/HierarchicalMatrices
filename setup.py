6bcb214fbcb9bb601efe64e5e1d487982fae68e1
Markus Neumann <markus.neumann@uzh.ch>
2017-03-28 10:00:56 +0200

    completed steup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,14 @@ from distutils.core import setup
 
 setup(
     name='HierMat',
-    version='',
+    version='0.01',
     packages=['HierMat'],
-    url='',
-    license='',
-    author='nem',
-    author_email='',
+    url='http://hierarchical-matrices.readthedocs.io/en/latest/HierMat.html',
+    download_url='https://git.math.uzh.ch/markus.neumann/HMatrix',
+    license='GPL',
+    author='Markus Neumann',
+    author_email='markus.neumann@math.uzh.ch',
     requires=['numpy', 'matplotlib'],
-    description=''
+    description='''This package is the result of my master thesis at the Institute of Mathematics, University of Zurich.
+It provides a framework for the concept of hierarchical matrices and is manly based on the book by W. Hackbusch.'''
 )
