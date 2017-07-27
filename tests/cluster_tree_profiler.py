ed67f0920b4bfc33734c5346389ff87340063e89
Markus Neumann <markus.neumann@uzh.ch>
2017-07-27 16:17:12 +0200

    changes to profiler

--- tests/cluster_tree_profiler.py
+++ tests/cluster_tree_profiler.py
@@ -10,7 +10,6 @@ from HierMat import *
 @profile
 def main(args):
     lim = args.limit
-    link_num = 2
     if args.dimension == 1:
         points = [((i + 0.5) / lim,) for i in xrange(lim)]
         supports = {points[i]: (points[i][0] - 0.5/lim, points[i][0] + 0.5/lim) for i in xrange(lim)}
@@ -29,31 +28,33 @@ def main(args):
     start = timer()
     ct_rc = ClusterTree(rc, 1)
     end = timer()
-    print "ClusterTree build-up with RegularCuboid took " + str(end - start)
+    print "ClusterTree build-up with RegularCuboid took {0} seconds.".format(end - start)
     start = timer()
     ct_mc = ClusterTree(mc, 1)
     end = timer()
-    print "ClusterTree build-up with MinimalCuboid took " + str(end - start)
+    print "ClusterTree build-up with MinimalCuboid took {0} seconds.".format(end - start)
     start = timer()
     ct_bc = ClusterTree(bc, 1)
     end = timer()
-    print "ClusterTree build-up with Balanced took " + str(end - start)
+    print "ClusterTree build-up with Balanced took {0} seconds.".format(end - start)
     start = timer()
     bct_rc_rc = BlockClusterTree(ct_rc, ct_rc)
     end = timer()
-    print "BlockClusterTree build-up with RegularCuboid took " + str(end - start)
+    print "BlockClusterTree build-up with RegularCuboid took {0} seconds." .format(end - start)
     start = timer()
     bct_mc_mc = BlockClusterTree(ct_mc, ct_mc)
     end = timer()
-    print "BlockClusterTree build-up with MinimalCuboid took " + str(end - start)
+    print "BlockClusterTree build-up with MinimalCuboid took {0} seconds.".format(end - start)
     start = timer()
     bct_bc_bc = BlockClusterTree(ct_bc, ct_bc)
     end = timer()
-    print "BlockClusterTree build-up with Balanced took " + str(end - start)
+    print "BlockClusterTree build-up with Balanced took {0} seconds.".format(end - start)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser("HMatrix Profiler")
     parser.add_argument("dimension", type=int, choices=[1, 2, 3])
     parser.add_argument("limit per dimension", type=int)
     args = parser.parse_args()
+    print '\n' + '*' * 74 + '\n'
     main(args)
+    print '\n' + '*' * 74 + '\n'
