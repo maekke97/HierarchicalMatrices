9d6952cdbd9390cc3d40a319069f39067d256c36
Markus Neumann <markus.neumann@uzh.ch>
2017-07-28 10:00:48 +0200

    cluster_tree_profiler.sh added

--- tests/cluster_tree_profiler.sh
+++ tests/cluster_tree_profiler.sh
@@ -2,5 +2,5 @@
 OUTFILE=/compute/nem/cluster_tree_profiler.out
 for e in `seq 2 24`;
 do
-    python cluster_tree_profiler.py 1 $(( 2**${e})) >> ${OUTFILE}
+    python cluster_tree_profiler.py 1 $(( 2**${e} )) >> ${OUTFILE}
 done
