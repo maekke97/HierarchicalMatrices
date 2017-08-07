476147ab26b15663d0c165a35b3212b597dd6ed3
Markus Neumann <markus.neumann@uzh.ch>
2017-08-07 13:18:10 +0200

    extended rmat analyzer

--- tests/plot_rmat_results.py
+++ tests/plot_rmat_results.py
@@ -14,22 +14,87 @@ with open(infile, 'r') as csvfile:
 
 fig = plt.figure()
 plt.title('Formatted addition with rank fixed')
-xs = [2**i for i in xrange(4, 25)]
+xs = [2**i for i in xrange(8, 18)]
 for k in xrange(1, 11):
     ax = fig.add_subplot(2, 5, k)
     times = [values[(x, k)] for x in xs]
     ax.plot(xs, times)
     plt.title('k={0}'.format(k))
 
-plt.show()
+plt.savefig('rmat_addition_k_profile.png')
 
 fig = plt.figure()
 plt.title('Formatted addition with n fixed')
 ks = range(1, 11)
-for i in xrange(4, 25):
-    ax = fig.add_subplot(3, 7, i-3)
+for i in xrange(8, 18):
+    ax = fig.add_subplot(2, 5, i-7)
     times = [values[(2**i, k)] for k in ks]
     ax.plot(ks, times)
     plt.title('n={0}'.format(2**i))
 
-plt.show()
+plt.savefig('rmat_addition_n_profile.png')
+
+
+infile = '/compute/nem/rmat_subtraction_profiler.csv'
+values = {}
+with open(infile, 'r') as csvfile:
+    value_reader = csv.reader(csvfile, delimiter=',')
+    for row in value_reader:
+        n = int(row[0])
+        k = int(row[1])
+        secs = float(row[2])
+        values[(n, k)] = secs
+
+fig = plt.figure()
+plt.title('Formatted subtraction with rank fixed')
+xs = [2**i for i in xrange(8, 18)]
+for k in xrange(1, 11):
+    ax = fig.add_subplot(2, 5, k)
+    times = [values[(x, k)] for x in xs]
+    ax.plot(xs, times)
+    plt.title('k={0}'.format(k))
+
+plt.savefig('rmat_subtraction_k_profile.png')
+
+fig = plt.figure()
+plt.title('Formatted subtraction with n fixed')
+ks = range(1, 11)
+for i in xrange(8, 18):
+    ax = fig.add_subplot(2, 5, i-7)
+    times = [values[(2**i, k)] for k in ks]
+    ax.plot(ks, times)
+    plt.title('n={0}'.format(2**i))
+
+plt.savefig('rmat_subtraction_n_profile.png')
+
+infile = '/compute/nem/rmat_multiplication_profiler.csv'
+values = {}
+with open(infile, 'r') as csvfile:
+    value_reader = csv.reader(csvfile, delimiter=',')
+    for row in value_reader:
+        n = int(row[0])
+        k = int(row[1])
+        secs = float(row[2])
+        values[(n, k)] = secs
+
+fig = plt.figure()
+plt.title('Multiplication with rank fixed')
+xs = [2**i for i in xrange(8, 18)]
+for k in xrange(1, 11):
+    ax = fig.add_subplot(2, 5, k)
+    times = [values[(x, k)] for x in xs]
+    ax.plot(xs, times)
+    plt.title('k={0}'.format(k))
+
+plt.savefig('rmat_multiplication_k_profile.png')
+
+fig = plt.figure()
+plt.title('Formatted multiplication with n fixed')
+ks = range(1, 11)
+for i in xrange(8, 18):
+    ax = fig.add_subplot(2, 5, i-7)
+    times = [values[(2**i, k)] for k in ks]
+    ax.plot(ks, times)
+    plt.title('n={0}'.format(2**i))
+
+plt.savefig('rmat_multiplication_n_profile.png')
--- tests/rmat_profiler.sh
+++ tests/rmat_profiler.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 OUTFILE=/compute/nem/rmat_profiler.out
+ADDCSV=/compute/nem/rmat_addition_profiler.csv
+SUBCSV=/compute/nem/rmat_subtraction_profiler.csv
+MULCSV=/compute/nem/rmat_multiplication_profiler.csv
 if [ -f ${OUTFILE} ];
 then
     rm -f ${OUTFILE}
 fi
-for e in `seq 4 24`;
+for e in `seq 8 17`;
 do
     for k in `seq 1 10`;
     do
         python rmat_profiler.py $(( 2**${e} )) ${k} >> ${OUTFILE}
     done
 done
+awk 'BEGIN{ OFS=","; FS="[ \t]+|=|" }/Addition/{ print $4, $7, $9}' ${OUTFILE} > ${ADDCSV}
+awk 'BEGIN{ OFS=","; FS="[ \t]+|=|" }/Subtraction/{ print $4, $7, $9}' ${OUTFILE} > ${SUBCSV}
+awk 'BEGIN{ OFS=","; FS="[ \t]+|=|" }/Multiplication/{ print $4, $7, $9}' ${OUTFILE} > ${MULCSV}
